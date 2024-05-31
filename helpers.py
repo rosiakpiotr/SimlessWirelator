5609944956cd0bca6205dc157421212826913eb0
Piotr Rosiak <rosiakpiotr3@gmail.com>
2024-05-31 12:15:47 +0200

    Added shortest path finding inside shape (needs more work)

--- helpers.py
+++ helpers.py
@@ -37,3 +37,38 @@ def read_equipment_csv(filename: str):
                     
 
     return equipment, obstacles
+
+from shapely.geometry import Point, LineString
+import networkx as nx
+from math import sqrt
+
+def shortest_path_in_polygon(polygon, start, end):
+    def is_visible(p1, p2, polygon):
+        line = LineString([p1, p2])
+        return polygon.contains(line)
+
+    def create_visibility_graph(polygon, start, end):
+        points = [start, end] + list(polygon.exterior.coords)
+        G = nx.Graph()
+
+        for i, p1 in enumerate(points):
+            for j, p2 in enumerate(points):
+                if i != j and is_visible(p1, p2, polygon):
+                    G.add_edge(tuple(p1), tuple(p2), weight=Point(p1).distance(Point(p2)))
+
+        return G
+    
+    def calculate_path_length(path):
+        length = 0.0
+        for i in range(len(path) - 1):
+            p1 = path[i]
+            p2 = path[i + 1]
+            length += sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
+        return length
+    
+    G = create_visibility_graph(polygon, start, end)
+    start = tuple(start)
+    end = tuple(end)
+
+    path = nx.shortest_path(G, source=start, target=end, weight='weight')
+    return calculate_path_length(path), path, G
